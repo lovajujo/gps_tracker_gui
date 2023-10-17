8796560a68b6f700828950bfec43cf96044dba04
lovajujo <lovaszi.zsuzsanna@gmail.com>
2023-10-17 12:44:36 +0200

    map OK, radio buttons; todo: stats, positions

---
+++ gpstracker.py
@@ -0,0 +1,70 @@
+import datetime
+
+import numpy as np
+import pandas as pd
+import folium
+from geopy import Point
+from geopy.distance import distance
+import tkintermapview
+
+
+def get_activity_date(df):
+    return pd.to_datetime(df[9].mode()[0]).strftime("%Y-%m-%d")
+
+
+def clear_df(df):
+    df = df[(df[0] == "$GPRMC") & (df[2] == "A")]
+    df = df[[1, 3, 4, 5, 6, 9]]
+    df = df.reset_index(drop=True)
+    df[1] = np.where(df[1].str.len() != 9, np.nan, df[1])
+    df[3] = np.where(df[3].str.len() != 10, np.nan, df[3])
+    df[4] = np.where(df[4].str.len() != 1, np.nan, df[4])
+    df[5] = np.where(df[5].str.len() != 11, np.nan, df[5])
+    df[6] = np.where(df[6].str.len() != 1, np.nan, df[6])
+    df[3] = np.where(df[4] == "S", "-" + df[3], df[3])
+    df[5] = np.where(df[6] == "W", "-" + df[5], df[5])
+    df[3] = df[3].dropna().apply(decimal_point_pos)
+    df[5] = df[5].dropna().apply(decimal_point_pos)
+    df['lat_dms'] = df[3].dropna().astype(float)
+    df['long_dms'] = df[5].dropna().astype(float)
+    df['lat_d'] = df[3].dropna().apply(convert_dms)
+    df['long_d'] = df[5].dropna().apply(convert_dms)
+    df['time_in_sec'] = df[1].dropna().apply(convert_time)
+    df = df.interpolate()
+    calculate_distance(df)
+    calculate_speed(df)
+    return df
+
+
+def calculate_distance(df):
+    df['point'] = df.apply(lambda row: Point(latitude=row['lat_d'], longitude=row['long_d']), axis=1)
+    df['point_next'] = df['point'].shift(1)
+    df['distance'] = df.apply(
+        lambda row: distance(row['point'], row['point_next']).m, axis=1)
+    return df
+
+
+def calculate_speed(df):
+    df['speed'] = df['distance'] / df['time_in_sec'].diff()
+    df['pace'] = (1000 / df['distance'] * df['time_in_sec']).rolling(3).mean()
+    df['speed_mps'] = df['speed'].rolling(3).mean()
+    df['speed_kmph'] = df['speed_mps'] / 3.6
+    df['speed_mpmin'] = df['speed_kmph'] * 1000 / 60
+
+
+def decimal_point_pos(c_str):
+    c_str = c_str.replace('.', '')
+    return f"{c_str[:-7]}.{c_str[-7:]}"
+
+
+def convert_dms(c_str):
+    d = c_str[:-8]
+    m = f"{c_str[-7:-5]}.{c_str[-5:]}"
+    return round(float(d) + float(m) / 60, 7)
+
+
+def convert_time(time):
+    h = str(time)[0:2]
+    m = str(time)[2:4]
+    s = str(time)[4:]
+    return (float(h) + 2) * 3600 + float(m) * 60 + float(s)
--- main.py
+++ main.py
@@ -1,18 +1,91 @@
+import datetime
 import tkinter as tk
-from tkinter import filedialog
+from tkinter import filedialog, Label
 import pandas as pd
+import tkintermapview
 
-df=""
-def open_file():
-    path=filedialog.askopenfilename()
-    df=pd.read_csv(path, names=range(13), on_bad_lines="skip")
-    label=tk.Label(root, text=df.shape)
-    label.pack()
+import gpstracker as gt
 
 root = tk.Tk()
-root.geometry("500x500")
+root.geometry("800x500")
 root.title("GPS tracker")
-file_button=tk.Button(text="choose gps log", command=open_file)
-file_button.pack()
-root.mainloop()
 
+date=""
+def open_file():
+    path = filedialog.askopenfilename()
+    df = pd.read_csv(path, names=range(13), on_bad_lines="skip")
+    date=gt.get_activity_date(df)
+    gpstracker(df)
+
+
+def gpstracker(df):
+    df = gt.clear_df(df)
+    df = gt.calculate_distance(df)
+    show_map(df)
+    #stats(df)
+
+
+def show_map(df):
+    map_label = tk.LabelFrame(root)
+    map_label.pack()
+    map_plot = tkintermapview.TkinterMapView(map_label, width=400, height=400, corner_radius=0)
+    points = list(zip(*map(df.get, ['lat_d', 'long_d'])))
+    map_plot.set_position(df['lat_d'].mean(), df['long_d'].mean())
+    map_plot.set_zoom(12)
+    path = map_plot.set_path(points)
+    map_plot.pack()
+
+
+def stats(df):
+    d_time=df.loc[len(df)-1][0]-df.loc[0][0]
+    duration=datetime.timedelta(seconds=d_time)
+    distance=df["distance"].sum()
+    if distance > 1000:
+        distance=f"{round(distance/1000,2)} km"
+    else:
+        distance=f"{int(distance)} m"
+    max_speed = f"{df['speed_kmph'].max()} km/h"
+    if activity_type==1:
+        avg_speed=f"{df['speed_mpmin'].mean()} meter/min"
+    elif activity_type==2:
+        avg_speed=f"{df['pace'].mean()} min/km"
+    else:
+        avg_speed=f"{df['speed_kmph'].mean()} km/h"
+    stats_label=tk.Label(root, justify=tk.RIGHT, padx=10, pady=10, text="Stats").pack()
+    tk.Label(stats_label, text=f"Activity date: {date, pd.to_datetime(df.iloc[0].loc['time_in_sec']).strftime('%H:%M:%S')}").pack()
+    tk.Label(stats_label, text=f"Duration: {duration}").pack()
+    tk.Label(stats_label, text=f"Average speed: {avg_speed}").pack()
+    tk.Label(stats_label, text=f"Max speed: {max_speed}")
+
+
+
+def show_speed(df):
+    pass
+
+
+def plot_speed_cycling(df):
+    pass
+
+
+def plot_speed_running(df):
+    pass
+
+
+def plot_speed_football(df):
+    pass
+
+
+file_button = tk.Button(text="choose gps log", command=open_file)
+file_button.pack()
+activity_type=tk.IntVar(root, 1)
+type_options={"Football": 1,
+              "Running": 2,
+              "Cycling": 3}
+for text, value in type_options.items():
+    tk.Radiobutton(root, text=text, variable=activity_type, value=value).pack()
+choose_type=tk.Radiobutton(root, )
+
+# log_date = pd.to_datetime(df[9].mode()[0]).strftime("%Y-%m-%d")
+
+
+root.mainloop()
