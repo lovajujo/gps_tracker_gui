2505e1d8eda0db5dbd3809206b42098de8ab1604
lovajujo <lovaszi.zsuzsanna@gmail.com>
2023-10-18 12:04:59 +0200

    works but wrong stats

--- main.py
+++ main.py
@@ -1,38 +1,46 @@
 import datetime
 import tkinter as tk
-from tkinter import filedialog
+from tkinter import filedialog, messagebox
 import pandas as pd
 import tkintermapview
 
 import gpstracker as gt
+
 
 class GPStracker(tk.Tk):
     def __init__(self):
         super().__init__()
         self.title("GPS tracker")
         self.geometry('800x600')
-        self.df=None
-        #load csv
-        self.file_button=tk.Button(text="Select gps log", font=("Sans", 14), command=self.open_file).pack()
-        # select activity type
+        self.df = None
+        self.date=None
+
+        # load csv
+        self.file_button = tk.Button(text="Select gps log", font=("Sans", 14), command=self.open_file).pack()
         self.selected_activity_type = tk.StringVar("")
         self.selected_activity_type.set("Select activity type")
-        self.options = ("Football", "Running", "Cycling")
+        options = ("Football", "Running", "Cycling")
+        tk.OptionMenu(self, self.selected_activity_type, *options).pack()
+        tk.Button(text="Show activity", command=self.show_activity).pack()
 
-    def select_activity_type(self):
-        self.activity_types = tk.OptionMenu(self, self.selected_activity_type, *self.options, command=self.show_stats).pack()
 
+
+    def show_activity(self):
+        if self.df is None or self.selected_activity_type.get() == "Select activity type":
+            messagebox.showerror("Python error","Select file and activity type")
+        else:
+            self.date=gt.get_activity_date(self.df)
+            self.df=gt.clear_df(self.df)
+            self.show_stats()
+            self.show_map()
 
     def open_file(self):
         path = filedialog.askopenfilename()
         if path:
             self.df = pd.read_csv(path, names=range(13), on_bad_lines="skip")
-            gt.get_activity_date(self.df)
-            self.select_activity_type()
 
     def show_stats(self):
-        gt.clear_df(self.df)
-        d_time = self.df.loc[len(self.df) - 1][0] - self.df.loc[0][0]
+        d_time = self.df['time_in_sec'].iloc[-1] - self.df['time_in_sec'].iloc[0]
         duration = datetime.timedelta(seconds=d_time)
         distance = self.df["distance"].sum()
         if distance > 1000:
@@ -47,13 +55,12 @@ class GPStracker(tk.Tk):
         else:
             avg_speed = f"{self.df['speed_kmph'].mean()} km/h"
         start_time = pd.to_datetime(self.df.iloc[0].loc['time_in_sec']).strftime('%H:%M:%S')
-        stats_label = tk.Label(self.root, justify=tk.RIGHT, padx=10, pady=10, text="Stats").pack()
+        stats_label = tk.Label(self, justify=tk.RIGHT, padx=10, pady=10, text="Stats").pack()
         tk.Label(stats_label, text=f"Activity date: {self.date, start_time}").pack()
         tk.Label(stats_label, text=f"Duration: {duration}").pack()
         tk.Label(stats_label, text=f"Distance covered: {distance}")
         tk.Label(stats_label, text=f"Average speed: {avg_speed}").pack()
         tk.Label(stats_label, text=f"Max speed: {max_speed}").pack()
-        self.show_map()
 
     def show_map(self):
         map_label = tk.LabelFrame(self)
@@ -67,5 +74,5 @@ class GPStracker(tk.Tk):
 
 
 if __name__ == '__main__':
-    app=GPStracker()
+    app = GPStracker()
     app.mainloop()
